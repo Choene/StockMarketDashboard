8aa8257bac9a888c28953e341760af37a397df32
Choene Chokoe <choene.chokoe@cyintecs.co.za>
2024-11-05 18:03:26 +0200

    Update deploy

--- StockMarketDashboard-FrontEnd/server.js
+++ StockMarketDashboard-FrontEnd/server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 // Serve the static files from the Angular build directory
-app.use(express.static(path.join(__dirname, 'dist/stock-market-dashboard-front-end')));
+app.use(express.static(path.join(__dirname, 'browser')));
 
 // Redirect all requests to index.html to enable Angular routing
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/stock-market-dashboard-front-end/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'browser', 'index.html'));
 });
 
 // Start the server
-const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
